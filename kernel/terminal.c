bd60b0bbc81ac05ba38741bb81145c9165265a01
RealAnomalyGames <146598908+RealAnomalyGames@users.noreply.github.com>
2026-08-11 15:03:45 +0000

    Add Atlas screen writing functions

--- kernel/terminal.c
+++ kernel/terminal.c
@@ -6,6 +6,7 @@
 static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
+
 static volatile uint16_t* const terminal_buffer =
     (uint16_t*)0xB8000;
 
@@ -25,16 +26,8 @@ static uint16_t vga_entry(
     return (uint16_t)character | (uint16_t)color << 8;
 }
 
-void terminal_initialize(void)
+static void terminal_clear(void)
 {
-    terminal_row = 0;
-    terminal_column = 0;
-
-    terminal_color = vga_entry_color(
-        VGA_COLOR_LIGHT_GREY,
-        VGA_COLOR_BLACK
-    );
-
     for (size_t y = 0; y < VGA_HEIGHT; y++)
     {
         for (size_t x = 0; x < VGA_WIDTH; x++)
@@ -47,44 +40,53 @@ void terminal_initialize(void)
     }
 }
 
+void terminal_initialize(void)
+{
+    terminal_row = 0;
+    terminal_column = 0;
+
+    terminal_color = vga_entry_color(
+        VGA_COLOR_LIGHT_GREY,
+        VGA_COLOR_BLACK
+    );
+
+    terminal_clear();
+}
+
 void terminal_putchar(char c)
 {
     if (c == '\n')
     {
         terminal_column = 0;
         terminal_row++;
+    }
+    else
+    {
+        const size_t index =
+            terminal_row * VGA_WIDTH + terminal_column;
 
-        if (terminal_row == VGA_HEIGHT)
+        terminal_buffer[index] =
+            vga_entry((unsigned char)c, terminal_color);
+
+        terminal_column++;
+
+        if (terminal_column == VGA_WIDTH)
         {
-            terminal_row = 0;
+            terminal_column = 0;
+            terminal_row++;
         }
-
-        return;
     }
 
-    const size_t index =
-        terminal_row * VGA_WIDTH + terminal_column;
-
-    terminal_buffer[index] =
-        vga_entry((unsigned char)c, terminal_color);
-
-    terminal_column++;
-
-    if (terminal_column == VGA_WIDTH)
+    if (terminal_row == VGA_HEIGHT)
     {
-        terminal_column = 0;
-        terminal_row++;
-
-        if (terminal_row == VGA_HEIGHT)
-        {
-            terminal_row = 0;
-        }
+        terminal_row = 0;
+        terminal_clear();
     }
 }
 
 void terminal_write(const char* data)
 {
-    while (*data)
+    while (*data != '\0')
     {
         terminal_putchar(*data);
         data++;
